77ea8840a6773fb5f1d086a7cc7f874420cd8f15
Eric Meyer <eemeyer@gmail.com>
2013-03-05 09:25:21 -0500

    Pulls out the API key portion of the URL to make chaning it for local development a bit easier.

--- js/search_results_film.js
+++ js/search_results_film.js
@@ -1,10 +1,11 @@
 (function ($, window) {
+    var apiKey = '&key=AIzaSyDf2i19WtZ53A9TiZ2W5zZ-otiAbR1hS8s';
     window.search_results = function (target, data) {
         var results = $('<div><div class="summary">There are <span class="exact"/> exact matches out of <span class="total"/> results</div></div>');
         var discoveryResponse = data._discovery.response;
         results.find(".exact").text(discoveryResponse.exactSize);
         results.find(".total").text(discoveryResponse.totalSize);
-        var imageUrlTemplate = 'https://usercontent.googleapis.com/freebase/v1/image{id}?maxheight=150&maxwidth=150&pad=true&key=AIzaSyDf2i19WtZ53A9TiZ2W5zZ-otiAbR1hS8s';
+        var imageUrlTemplate = 'https://usercontent.googleapis.com/freebase/v1/image{id}?maxheight=150&maxwidth=150&pad=true' + apiKey;
         $.each(discoveryResponse.itemIds, function (itemIndex, itemId) {
             var item = {
                 '_id': itemId,
