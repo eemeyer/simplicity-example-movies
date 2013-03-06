b3c3e0c7df719af4804a4ea93cf0fb8229176afa
Eric Meyer <eemeyer@gmail.com>
2013-03-06 15:31:37 -0500

    Makes rating and runtime both simplicitySilders with hidden inputs for their values. Adds labels for each with a textual description of the criteria range.

--- js/search_page.js
+++ js/search_page.js
@@ -1,28 +1,57 @@
 $(function() {
-    $('body').simplicityState();
-    $('#q,#genre,#rating').simplicityInputs();
-    $('#initial_release_year select').simplicityInputs().attr('disabled', 'disabled');
-    $('#initial_release_year select:first').each(function() {
-        var yr = 1888;
-        var currentYear = new Date().getFullYear();
-        for (; yr <= currentYear; ++yr) {
-            $(this).append($('<option/>').text(yr));
+    $('input[name="runtime_min"],input[name="runtime_max"]').bind('change', function (evt) {
+        var message = '';
+        var min = $('input[name="runtime_min"]').val();
+        var max = $('input[name="runtime_max"]').val();
+        if (min === '' && max === '') {
+            message = 'any length';
+        } else if (min === '') {
+            message = 'up to ' + minutesToText(max);
+        } else if (max === '') {
+            message = 'at least ' + minutesToText(min);
+        } else {
+            message = 'from ' + minutesToText(min) + ' to ' + minutesToText(max);
         }
+        $('#runtime_criteria_desc').text(message);
+    }).change();
+    $('input[name="initial_release_year_min"],input[name="initial_release_year_max"]').bind('change', function (evt) {
+        var message = '';
+        var min = $('input[name="initial_release_year_min"]').val();
+        var max = $('input[name="initial_release_year_max"]').val();
+        if (min === '' && max === '') {
+            message = 'in any year';
+        } else if (min === '') {
+            message = 'through ' + max;
+        } else if (max === '') {
+            message = 'after ' + min;
+        } else {
+            message = '' + min + ' - ' + max;
+        }
+        $('#initial_release_year_criteria_desc').text(message);
+    }).change();
+
+    $('body').simplicityState();
+    $('' +
+        '#q,#genre,#rating,' +
+        'input[name="initial_release_year_min"],input[name="initial_release_year_max"],' +
+        'input[name="runtime_min"],input[name="runtime_max"]'
+    ).simplicityInputs();
+    var currentYear = new Date().getFullYear();
+    $('#initial_release_year_slider').simplicitySlider({
+        input: ['input[name="initial_release_year_min"]', 'input[name="initial_release_year_max"]'],
+        min: 1887,
+        max: currentYear + 1,
+        values: [1888, currentYear],
+        any:[1887, currentYear + 1],
+        range: true
     });
-    $('#initial_release_year_slider').simplicitySelectSlider({
-        select:  '#initial_release_year select[name="initial_release_year_min"]',
-        secondSelect: '#initial_release_year select[name="initial_release_year_max"]',
-        showLabels: false,
-        showTooltip: false,
-        populateSecondSelect: true
-    });
-    $('input[name="runtime_min"],input[name="runtime_max"]').simplicityInputs().attr('disabled', 'disabled');
     $('#runtime_slider').simplicitySlider({
       input: ['input[name="runtime_min"]', 'input[name="runtime_max"]'],
       min: 0,
       max: 361,
       values: [0,360],
-      any: [0, 361]
+      any: [0, 361],
+      range: true
     });
     $('#genre,#rating').simplicityFacetedSelect().hide();
     $('#genre_fancy').simplicityFancySelect({
